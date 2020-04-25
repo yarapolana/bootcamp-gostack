8e5da67fd689789d2ef3ba3f7fa5526a362a5690
yarapolana <yara.polana@gmail.com>
2020-04-25 19:36:37 +0000

    Initiated challenge

---
+++ nivel-1/node/_desafio/src/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const logRequests = require("./middlewares/logRequest");
+const validateRepositoryId = require("./middlewares/validateRepositoryId");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const repositories = [];
+
+app.use(logRequests);
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repo = { id: uuid(), title, url, techs: [] };
+
+  repositories.push(repo);
+
+  return response.json(repo);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs: [],
+  };
+
+  repositories[repoIndex] = repo;
+
+  return response.json(repo);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  // TODO
+});
+
+module.exports = app;
